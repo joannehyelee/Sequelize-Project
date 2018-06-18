dd83a25385ac04d29716630a6d58434f72091590
Joanne Kim <joannehyelee@gmail.com>
2018-06-17 23:59:25 -0700

    Create product controller

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -2,4 +2,18 @@
 
 exports.index = function(req, res) {
     res.render('products/products');
+
+    // GET route for getting all of the products
+    // db.Product.findAll({})
+    //     .then(function(dbProduct) {
+    //         console.log(dbProduct);
+    //         res.render('products/products', {
+
+    //         });
+    //     });
+
+};
+
+exports.createProduct = function(req, res) {
+
 };
--- routes/products.js
+++ routes/products.js
@@ -6,4 +6,7 @@ var products_controller = require('../controllers/products_controller');
 router.get('/', products_controller.index);
 // localhost:8000/products
 
+router.post('/new', products_controller.createProduct);
+// localhost:8000/products/new
+
 module.exports = router;
