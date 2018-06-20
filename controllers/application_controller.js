034f896ac084d0b3a3b3815362c9efa6d3d2afbf
Jessica Cervantes <J1123C@gmail.com>
2018-06-19 23:54:21 -0700

    wireup home page
    Signed-off-by: Jessica Cervantes <J1123C@gmail.com>

--- controllers/application_controller.js
+++ controllers/application_controller.js
@@ -1,3 +1,13 @@
+var db = require('../models');
+
 exports.index = function(req, res) {
-    res.render('index');
+
+    // get all games
+    db.Game.findAll({})
+        .then(function (Games) {
+            //console.log(Games);
+            res.render('index', {
+                games: Games
+            });
+        });
 };
--- db-seeds.sql
+++ db-seeds.sql
@@ -1,4 +1,6 @@
 INSERT INTO `Games` (`id`, `name`, `description`, `price`, `quantity`, `imageURL`, `productCode`, `createdAt`, `updatedAt`)
 VALUES
 	(1,'Mario Kart 8 Deluxe','Fun racing game for kids',60,50,'https://www.gamestop.com/common/images/lbox/141784b.jpg','03213741','2018-06-20 06:09:53','2018-06-20 06:09:53'),
-	(2,'Kirby Star Allies','Fun with Kirby',60,10,'https://www.gamestop.com/common/images/lbox/161435b.jpg','03213432','2018-06-20 06:09:53','2018-06-20 06:09:53');
+	(2,'Kirby Star Allies','Fun with Kirby',58,10,'https://www.gamestop.com/common/images/lbox/161435b.jpg','03213432','2018-06-20 06:09:53','2018-06-20 06:09:53'),
+	(3,'Streetfighter','Go punch somebody',70,35,'https://vignette.wikia.nocookie.net/nintendo/images/6/63/Street_Fighter_II_%28GB%29_box_art.jpg/revision/latest?cb=20161010125950&path-prefix=en','03213431','2018-06-20 06:09:53','2018-06-20 06:09:53'),
+	(4,'Mortal Kombat','Komplete Edition',80,5,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoEZXEOyiwatgshKLXjoByrEMuqtVsYI1vno-5Ti09Iej6Ftlj','03213433','2018-06-20 06:09:53','2018-06-20 06:09:53');
